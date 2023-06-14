cdc8ceae634e81d7a6577ef134aa56b9f534dbe9
Your Name <you@example.com>
2023-06-14 21:59:01 +0530

    First commit for task 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  //adding showGraph peroperty and set it to boolean
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      //setting initial state of graph as hidden
+      showGraph: false;
     };
   }
 
@@ -29,18 +33,30 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    //make graph rendering as conditional 
+    if(this.state.showGraph){
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+    //added loop to get data from server continuously
+    let x = 0;
+    const interval = setInterval((){
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({ 
+          data: serverResponds,
+          showGraph: true,        
+        });
     });
+    x++;
+    if(x>1000){
+      clear Interval(interval);
+     }
+    },100);
   }
 
   /**
